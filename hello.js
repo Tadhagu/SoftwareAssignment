62f14e02b200ab12b8771b0ecae773b66d41bd9e
Tadhg Cuneo <t.cuneo@education.nsw.gov.au>
2023-01-23 20:51:04 +1100

    Added the first bit of js to the project, retrieving the data from the API, and adding the first bit to the HTML document, originally done on 1/8/2023

--- hello.js
+++ hello.js
@@ -1,1 +1,24 @@
-console.log("hello");
+let Data = {
+    fetchData: function () {
+        fetch("https://v3.football.api-sports.io/teams?id=33", {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "v3.football.api-sports.io",
+                "x-rapidapi-key": "aefedc74219709b46c5b8bcded1a0d04"
+            }
+        }).then(response => response.json()
+        ).then((data) => this.displayData(data))
+            .catch(error => console.log('error', error));
+
+    },
+    displayData: function (data) {
+        const test = data['response']
+        const {name, logo, country} = test[0]['team'];
+        console.log(name, logo, country)
+        document.querySelector(".team_name").innerText = name
+        //document.querySelector(".team_icon").src = logo;
+    }
+};
+
+
+
